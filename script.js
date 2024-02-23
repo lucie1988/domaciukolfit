e4429aebc3f1b8649d05b704891a39b3c8ded2d1
Lucie Panagopulos <luciepanagopulos@gmail.com>
2024-02-23 21:01:00 +0100

    oprava domaciho ukolu

--- script.js
+++ script.js
@@ -1,13 +1,6 @@
 let krestniJmeno = prompt("Zadejte křestní jméno bez diakritiky")
 let prijmeni = prompt("Zadejte příjmení bez diakritiky")
 const domain = "@fit.cvut.cz"
-document.body.innerHTML = krestniJmeno.trimStart()
-document.body.innerHTML = krestniJmeno.trimEnd()
-document.body.innerHTML = prijmeni.trimStart()
-document.body.innerHTML = prijmeni.trimEnd()
-document.body.innerHTML = krestniJmeno.slice(0,3)
-document.body.innerHTML = prijmeni.slice (0,5)
-document.body.innerHTML = krestniJmeno.toLowerCase()
-document.body.innerHTML = prijmeni.toLowerCase()
 
-document.body.innerHTML = `Email: ${krestniJmeno}${prijmeni}${domain} `
+
+document.body.innerHTML = `email:${prijmeni.trim().slice(0,5).toLowerCase()}${krestniJmeno.trim().slice(0,3).toLowerCase()}${domain}`
